781fe136f2cd5a39b3b084b5c7588e91480ad072
wimedina01 <61916245+wimedina01@users.noreply.github.com>
2026-03-27 12:24:13 -0500

    h

--- src/components/SyncBar.jsx
+++
@@ -1,9 +0,0 @@
-export default function SyncBar({ state, text }) {
-  if (!state) return null
-  return (
-    <div className={`sync-bar show ${state}`}>
-      {state === 'syncing' && <div className="spinner" />}
-      <span>{text}</span>
-    </div>
-  )
-}
